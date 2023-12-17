94c4e3965f8c7cd2229fa1eaaf3ab8048eb71f0f
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-12-17 17:06:53 +0100

    feat(AboutImagesSection): add AboutImagesSection component()

---
+++ src/components/AboutImagesSection/AboutImagesSection.tsx
@@ -0,0 +1,17 @@
+const AboutImagesSection = () => {
+  return (
+    <section className="flex gap-4 m-10">
+      <div className="flex-1 flex items-center">
+        <img src="../../../public/images/aboutUnplash1.png" className="h-full object-cover" />
+      </div>
+      <div className="flex-1 flex items-center">
+        <img src="../../../public/images/aboutUnplash2.png" className="h-full object-cover" />
+      </div>
+      <div className="flex-1 flex items-center">
+        <img src="../../../public/images/aboutUnplash3.png" className="h-full object-cover" />
+      </div>
+    </section>
+  );
+};
+
+export default AboutImagesSection;
