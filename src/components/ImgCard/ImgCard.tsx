acf3fa41d1a6d10d14e16de61f514111ad3e53ef
Piotr Łosiak <piotr.losiak96@gmail.com>
2024-01-14 19:25:23 +0100

    chore: refactor ImgCard types and component

--- src/components/ImgCard/ImgCard.tsx
+++ src/components/ImgCard/ImgCard.tsx
@@ -1,40 +1,48 @@
 import React from 'react';
 
-interface ImgCardProps {
-  slug: string;
+interface BaseImgCardProps {
   title: string;
   imgSrc: string;
   imgClassName?: string;
-  as?: 'link' | 'button';
+}
+
+interface ImgCardAsLink extends BaseImgCardProps {
+  as?: 'link';
+  slug: string;
+  onClick?: never;
+}
+
+interface ImgCardAsButton extends BaseImgCardProps {
+  as?: 'button';
+  slug?: never;
   onClick: React.MouseEventHandler;
 }
 
-// todo: implement as props: when as equals link card should have anchor element
-// and when as equals button it should have button
+type ImgCardProps = ImgCardAsLink | ImgCardAsButton;
+
 const ImgCard = ({ slug, title, imgSrc, imgClassName = '', as = 'link', onClick }: ImgCardProps) => {
+  const children = (
+    <>
+      <p className="text-center text-xl font-bold mb-2">{title}</p>
+      <div className={`relative aspect-square h-full ${imgClassName}`}>
+        <img
+          className="rounded-lg object-cover transition duration-300 transform hover:scale-105 absolute left-0 top-0 w-full h-full"
+          alt="iphone-photo"
+          src={imgSrc}
+        />
+      </div>
+    </>
+  );
+
   return (
     <div className=" flex-1">
       {as === 'link' ? (
         <a href={slug} className="flex flex-col justify-between items-center relative h-full">
-          <p className="text-center text-xl font-bold mb-2">{title}</p>
-          <div className={`relative aspect-square h-full ${imgClassName}`}>
-            <img
-              className="rounded-lg object-cover transition duration-300 transform hover:scale-105 absolute left-0 top-0 w-full h-full"
-              alt="iphone-photo"
-              src={imgSrc}
-            />
-          </div>
+          {children}
         </a>
       ) : (
         <button onClick={onClick} className="flex flex-col justify-between items-center relative h-full">
-          <p className="text-center text-xl font-bold mb-2">{title}</p>
-          <div className={`relative aspect-square h-full ${imgClassName}`}>
-            <img
-              className="rounded-lg object-cover transition duration-300 transform hover:scale-105 absolute left-0 top-0 w-full h-full"
-              alt="iphone-photo"
-              src={imgSrc}
-            />
-          </div>
+          {children}
         </button>
       )}
     </div>
