2b38d192aa561228013f525b5d29e853f05ceb4e
Mateusz-95 <122434234+Mateusz-95@users.noreply.github.com>
2024-01-31 08:50:44 +0100

    Feature/mobile view adjustment (#5)
    * feat(): mobile view in models, not full responsive yet
    
    * feat(): mobile view in models, complete
    
    * fix: improve logo resolution
    
    * fix: change duration time in phones objects
    
    * feat: add contact email button and update repair times

--- src/components/Navigation/MainNav.tsx
+++ src/components/Navigation/MainNav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import HamburgerButton from './hamburger-button.tsx';
 import MobileMenu from './mobile-menu.tsx';
 
@@ -23,10 +23,10 @@ const MainNav = () => {
             <a href="/about/">O nas</a>
           </li>
         </ul>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <li>
             <a
-              className="hidden md:flex mr-2"
+              className="hidden md:flex"
               href="https://www.instagram.com/spacefiix/"
               target="_blank"
               rel="noopener noreferrer"
@@ -36,12 +36,21 @@ const MainNav = () => {
           </li>
           <li>
             <a
-              className="mr-2 hidden md:flex"
+              className="hidden md:flex"
               href="https://www.facebook.com/profile.php?id=100094392695973"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img alt="facebook icon" src="/images/facebook.png" className="w-10 h-10" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:biurospacefix@gmail.com?subject=Potrzebuję%20naprawy!&body=Potrzebuję%20naprawy!%0D%0APoniżej%20podaję%20jaki%20sprzęt%20potrzebuje%20Waszej%20naprawy%20i%20co%20dokładnie%20mu%20dolega!%20:)%0D%0AZostawiam%20również%20numer%20telefonu%20pod%20który%20zadzwonicie%20z%20wyceną:"
+              className="flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-2 md:text-lg md:px-4 transition duration-150 ease-in-out"
+            >
+              <img alt="mail icon" src="/public/images/mail.png" className="mr-2 h-6 w-6" />
+              <span className="hidden md:block">Poproś o wycenę</span>
             </a>
           </li>
         </ul>
