1aa30c4b3d7151a2b383089dac7c955c888655d5
Piotr Losiak <47902696+moose96@users.noreply.github.com>
2024-02-08 20:38:56 +0100

    feat: refactor apple categories (#9)

---
+++ src/data/iphone/categories.ts
@@ -0,0 +1,10 @@
+import iphoneModels from './models.ts';
+import { appleWatchModels } from '../appleWatch/models.ts';
+
+const appleCategories = [
+  { name: 'iPhone', slug: 'iphone/', imgSrc: '/images/iphone-photo.png', phones: iphoneModels },
+  { name: 'iPad', slug: '/offer/apple-ipad/', imgSrc: '/images/ipad-photo.png', phones: [], ignore: 'pages' },
+  { name: 'Apple Watch', slug: 'apple-watch/', imgSrc: '/images/applewatch-photo.png', phones: appleWatchModels },
+];
+
+export default appleCategories;
--- src/templates/image-card-grid.tsx
+++ src/templates/image-card-grid.tsx
@@ -14,16 +14,20 @@ const ImageCardGrid: React.FC<ImageCardGridProps> = ({ headerContent, data }) =>
   return (
     <section className="mt-5">
       <h2 className="text-center text-3xl font-bold mb-4">{headerContent}</h2>
-      <div className="max-md:flex md:h-[500px] mb-10 lg:flex lg:h-[250px] md:grid md:grid-cols-3 max-md:flex-col justify-center gap-6 md:gap-0">
-        {data.map(({ slug, name, imgSrc }) => {
-          return <ImgCard key={slug} slug={slug} title={name} imgSrc={imgSrc} />;
-        })}
+      <div className="max-md:flex md:h-[350px] mb-10 xl:flex xl:h-[250px] md:grid md:grid-cols-3 max-md:flex-col justify-center gap-6 md:gap-0">
+        {data
+          .filter((item) => item.ignore !== 'list')
+          .map(({ slug, name, imgSrc }) => {
+            return <ImgCard key={slug} slug={slug} title={name} imgSrc={imgSrc} />;
+          })}
       </div>
       <div className="text-center">
         <h3 className="text-2xl font-bold mb-8">Nie znalazłeś swojego modelu?</h3>
         <a href="tel:+48730889759">
           <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
-            Zadzwoń, chętnie pomożemy: +48 730 889 759
+            Zadzwoń, chętnie pomożemy:
+            <br className="md:hidden block" />
+            +48 730 889 759
           </button>
         </a>
       </div>
