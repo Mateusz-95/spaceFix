ac03284d68b79e95d87a28b3fd4c0834774370c8
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2024-02-08 20:56:18 +0100

    fix: icon src repaired

--- src/components/Navigation/MainNav.tsx
+++ src/components/Navigation/MainNav.tsx
@@ -49,7 +49,7 @@ const MainNav = () => {
               href="mailto:biurospacefix@gmail.com?subject=Potrzebuję%20naprawy!&body=Potrzebuję%20naprawy!%0D%0APoniżej%20podaję%20jaki%20sprzęt%20potrzebuje%20Waszej%20naprawy%20i%20co%20dokładnie%20mu%20dolega!%20:)%0D%0AZostawiam%20również%20numer%20telefonu%20pod%20który%20zadzwonicie%20z%20wyceną:"
               className="flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-2 md:text-lg md:px-4 transition duration-150 ease-in-out"
             >
-              <img alt="mail icon" src="/public/images/mail.png" className="mr-2 h-6 w-6" />
+              <img alt="mail icon" src="/images/mail.png" className="mr-2 h-6 w-6" />
               <span className="hidden md:block">Poproś o wycenę</span>
             </a>
           </li>
